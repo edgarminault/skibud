39b4b8261d84998d0591dd73f1d57252c94ec66c
Edgar Minault <e.minault@bluewin.ch>
2020-06-18 13:56:55 +0200

    Routes added.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only:[:show, :edit, :update]
+  resources :itineraries, only:[:show, :edit, :update] do
+    resources :activities, only:[:show, :edit, :update]
+  end
 end
