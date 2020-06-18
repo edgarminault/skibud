29764e71215d5d5caac0ee1c95f69d67b813f9da
Edgar Minault <e.minault@bluewin.ch>
2020-06-18 15:21:03 +0200

    Seed for itineraries started.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,25 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+# Seed for itineraries
+
+itineraries_url = 'https://api.camptocamp.org/routes?act=skitouring'
+raw_itineraries = open(itineraries_url).read
+parsed_itineraries = JSON.parse(raw_itineraries)
+parsed_itineraries["documents"].each do |itinerary|
+  title =  itinerary["locales"][0]["title_prefix"]
+
+  # Scrape webpage info of the given itinerary
+  itinerary_url = "https://www.camptocamp.org/routes/#{itinerary["document_id"]}"
+  p itinerary_url
+  html = open(itinerary_url)
+  doc = Nokogiri::HTML(html)
+  html = doc.css('p')
+  binding.pry
+end
