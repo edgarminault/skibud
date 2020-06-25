a6d0fa77817c832e551e8f967bee0f0642e8ecfe
Edgar Minault <e.minault@bluewin.ch>
2020-06-25 11:43:19 +0200

    Contry code inserted.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_090515) do
+ActiveRecord::Schema.define(version: 2020_06_25_093600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2020_06_25_090515) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["itinerary_id"], name: "index_activities_on_itinerary_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "title"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "itineraries", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,52 +7,59 @@
 # Seed for itineraries
 require 'json'
 require 'open-uri'
+require 'nokogiri'
 
-puts "Cleaning database..."
-Itinerary.destroy_all
+# puts "Cleaning database..."
+# Itinerary.destroy_all
+# Country.destroy_all
 
-puts "Creating itineraries..."
+# puts "Creating itineraries..."
 
-# Import the JSON file in the seed
-path = File.join(File.dirname(__FILE__), "./seeds/skitours.json")
-itineraries = JSON.parse(File.read(path))["beers"]["data"]
-i = 1
-itineraries.each do |itinerary|
-  # Check for the outdoor active api route id and scrape the webpage.
-  itinerary_id = itinerary["-id"]
-  itinerary_url = "http://www.outdooractive.com/api/project/api-dev-oa/oois/#{itinerary_id}?key=yourtest-outdoora-ctiveapi"
-  raw_itinerary = HTTParty.get(itinerary_url)
+# # Import the JSON file in the seed
+# path = File.join(File.dirname(__FILE__), "./seeds/skitours.json")
+# itineraries = JSON.parse(File.read(path))["beers"]["data"]
+# i = 1
+# itineraries.each do |itinerary|
+#   # Check for the outdoor active api route id and scrape the webpage.
+#   itinerary_id = itinerary["-id"]
+#   itinerary_url = "http://www.outdooractive.com/api/project/api-dev-oa/oois/#{itinerary_id}?key=yourtest-outdoora-ctiveapi"
+#   raw_itinerary = HTTParty.get(itinerary_url)
 
-  # Parsing the raw answer to a hash with HTTParty function parsed_response.
-  parsed_itinerary = raw_itinerary.parsed_response
-  title = parsed_itinerary["oois"]["tour"]["title"]
-  description = parsed_itinerary["oois"]["tour"]["longText"]
-  poi = parsed_itinerary["oois"]["tour"]["geometry"].split(" ")
-  hashed_poi = Hash.new
-  hashed_components = Hash.new
-  counter = 1
-  poi.each do |point|
-    # Each point is a hash of lat, long and alt. All points are stored in a hash.
-    hashed_components["long"] = point.split(",")[0].to_f
-    hashed_components["lat"] = point.split(",")[1].to_f
-    hashed_components["alt"] = point.split(",")[2].to_i
-    hashed_poi[counter] = hashed_components
-    counter += 1
-  end
+#   # Parsing the raw answer to a hash with HTTParty function parsed_response.
+#   parsed_itinerary = raw_itinerary.parsed_response
+#   title = parsed_itinerary["oois"]["tour"]["title"]
+#   description = parsed_itinerary["oois"]["tour"]["longText"]
+#   poi = parsed_itinerary["oois"]["tour"]["geometry"].split(" ")
+#   hashed_poi = Hash.new
+#   hashed_components = Hash.new
+#   counter = 1
+#   poi.each do |point|
+#     # Each point is a hash of lat, long and alt. All points are stored in a hash.
+#     hashed_components["long"] = point.split(",")[0].to_f
+#     hashed_components["lat"] = point.split(",")[1].to_f
+#     hashed_components["alt"] = point.split(",")[2].to_i
+#     hashed_poi[counter] = hashed_components
+#     counter += 1
+#   end
 
-  latitude = hashed_poi[1]["lat"]
-  longitude = hashed_poi[1]["long"]
-  itinerary = Itinerary.new(
-    title: title,
-    description: description,
-    latitude: latitude,
-    longitude: longitude,
-    poi: hashed_poi
-  )
-  itinerary.save!
-  p "Itinerary #{i} - #{title} - imported."
-  i += 1
+#   latitude = hashed_poi[1]["lat"]
+#   longitude = hashed_poi[1]["long"]
+#   itinerary = Itinerary.new(
+#     title: title,
+#     description: description,
+#     latitude: latitude,
+#     longitude: longitude,
+#     poi: hashed_poi
+#   )
+#   itinerary.save!
+#   p "Itinerary #{i} - #{title} - imported."
+#   i += 1
 
-end
-puts "countries are seeded"
+# end
+# puts "Itineraries are seeded"
+
+puts "Creating countries..."
+
+countries_url = 'https://gist.githubusercontent.com/keeguon/2310008/raw/bdc2ce1c1e3f28f9cab5b4393c7549f38361be4e/countries.json'
+
 
