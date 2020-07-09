2c3f95938116b4a2b1d678aedb30dccbe9612c0c
Edgar Minault <e.minault@bluewin.ch>
2020-07-09 20:37:18 +0200

    Markers created and map-container inserted.

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,5 +1,11 @@
 class ItinerariesController < ApplicationController
   def index
     @itineraries = Itinerary.order("RANDOM()").all
+    @markers = @flats.map do |map|
+      {
+        lat: flat.latitude,
+        long: flat.lomngitude
+      }
+    end
   end
 end
