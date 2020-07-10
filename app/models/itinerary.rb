2d67a81775514f3983eee68bb06e246ba39fdc19
Edgar Minault <e.minault@bluewin.ch>
2020-07-10 11:14:38 +0200

    Geocoding done.

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,5 +1,4 @@
 class Itinerary < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
-  infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
 end
